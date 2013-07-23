bb294e2459fc53227a058cc627b0d241707c9b06
James Broad <jb@kulor.com>
2013-07-23 22:42:01 +0100

    Adding code and comments for how to structure a Backbone View in TypeScript. Mainly that prototype members need to live inside a constructor and be followed by a super() call

--- lib/greeter.ts
+++ lib/greeter.ts
@@ -2,17 +2,30 @@
 /// <reference path="../d.ts/jquery.d.ts" />
 /// <reference path="../d.ts/backbone.d.ts" />
 /// <reference path="../d.ts/require.d.ts" />
+'use strict';
 var Backbone = require('backbone');
 var $ = require('jquery');
 
 export class Greeter extends Backbone.View {
-    el: HTMLElement;
+    // Private message only accesable to each view instance
     greetMessage: string;
 
     constructor () {
+        // Attach your prototype members here to be avaialable
+        // before calling super() which will wipe your settings
         this.el = $('#app');
+        this.events = {
+            'click': 'clickHandler'
+        }
+
+        // Ensure super is called last or your View won't work properly
         super ();
     }
+    
+    clickHandler (e) {
+        console.log('element clicked', e);
+    }
+
     greet (name: string) {
         this.greetMessage = "Hello, " + name;
         return this.greetMessage;
